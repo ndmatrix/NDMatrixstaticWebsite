c41ddf164bfa5e24755cb6cd4cb5b1d5b733677d
chinnari-2002 <chinnari.a@egradtutor.in>
2025-06-26 10:04:59 +0530

    changes in the ndmatrix page

--- ndmatrix/src/app/components/n-dmatrix-home-page/n-dmatrix-home-page.ts
+++ ndmatrix/src/app/components/n-dmatrix-home-page/n-dmatrix-home-page.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
 import { MainHeader } from '../main-header/main-header';
 import { MainFooter } from '../main-footer/main-footer';
 import { MainBanner } from '../main-banner/main-banner';
 import { VerticalsPage } from '../verticals-page/verticals-page';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'n-dmatrix-home-page',
@@ -11,7 +12,27 @@ import { VerticalsPage } from '../verticals-page/verticals-page';
   templateUrl: './n-dmatrix-home-page.html',
   styleUrls:[ './n-dmatrix-home-page.css']
 })
-export class NDMatrixHomePage {
+export class NDMatrixHomePage implements AfterViewInit{
 
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  @ViewChild('verticalsContainer', { static: false }) verticalsContainer!: ElementRef;
+
+  ngAfterViewInit() {
+        if (isPlatformBrowser(this.platformId)) {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('verticals-visible');
+        } else {
+          entry.target.classList.remove('verticals-visible');
+        }
+      });
+    }, { threshold: 0.1 });
+
+    if (this.verticalsContainer?.nativeElement) {
+      observer.observe(this.verticalsContainer.nativeElement);
+    }
+  }
+}
 
 }
