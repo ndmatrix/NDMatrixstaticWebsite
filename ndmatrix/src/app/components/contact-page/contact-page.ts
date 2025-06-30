1c932f0a140eb0674029d90a70c60676d15413aa
chinnari-2002 <chinnari.a@egradtutor.in>
2025-06-30 18:02:05 +0530

    changes in the career pages and  products

--- ndmatrix/src/app/components/contact-page/contact-page.ts
+++ ndmatrix/src/app/components/contact-page/contact-page.ts
@@ -14,29 +14,4 @@ const isBrowser = typeof window !== 'undefined';
   styleUrls: ['./contact-page.css']
 })
 export class ContactPage {
-  // @ViewChild('contactFormRef') contactFormRef!: ElementRef<HTMLFormElement>;
-
-  // sendEmail() {
-  //   console.log("sendEmail() triggered");
-
-  //   emailjs.sendForm(
-  //     'service_2ap83yn',
-  //     'template_1ffd92k',
-  //     this.contactFormRef.nativeElement,
-  //     'FTTyjdyRLojv6u0PZ'
-  //   ).then(() => {
-  //     alert('Message sent successfully!');
-  //     this.contactFormRef.nativeElement.reset();
-
-  //     // Optional: Clear scroll target after submitting form
-  //     // if (isBrowser) {
-  //     //   window.sessionStorage.removeItem('scrollTarget');
-  //     //   window.sessionStorage.removeItem('activeButton');
-  //     // }
-
-  //   }).catch((error) => {
-  //     console.error("EmailJS Error:", error);
-  //     alert('Failed to send message. Please check form fields.');
-  //   });
-  // }
 }
